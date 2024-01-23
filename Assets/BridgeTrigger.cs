416bd79b286131f42f029d37a29baf014da4fe3f
STHAOPHAN <phansongthao203@gmail.com>
2024-01-23 15:32:50 +0700

    update build mode, fix layer for hill tiles

---
+++ Assets/BridgeTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeTrigger : MonoBehaviour
+{
+    private bool playerOnBridge = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !playerOnBridge)
+        {
+            ToggleTrigger(true);
+            playerOnBridge = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && playerOnBridge)
+        {
+            ToggleTrigger(false);
+            playerOnBridge = false;
+        }
+    }
+
+    void ToggleTrigger(bool enable)
+    {
+        GameObject[] platformUnderBridges = GameObject.FindGameObjectsWithTag("Platform Under Bridge");
+        foreach (GameObject platform in platformUnderBridges)
+        {
+            Collider2D[] colliders = platform.GetComponents<Collider2D>();
+
+            foreach (Collider2D collider in colliders)
+            {
+                collider.enabled = !enable;
+            }
+        }
+    }
+}
