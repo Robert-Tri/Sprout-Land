e6485167937d3382a97d8f2f16f5182db5ba209e
STHAOPHAN <phansongthao203@gmail.com>
2024-01-30 15:39:19 +0700

    cp

--- Assets/InteractableObject.cs
+++ Assets/InteractableObject.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class InteractableObject : MonoBehaviour
 {
     private bool isPlayerInRange = false;
     private TextMesh interactText;
     private GameObject textObject;
+    public Tilemap tilemap;
+    public TileBase newTile;
+    public GameObject objectToCreate;
 
     private void Start()
     {
@@ -29,8 +33,20 @@ public class InteractableObject : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            isPlayerInRange = true;
-            interactText.gameObject.SetActive(true);
+            Vector3Int cellPosition = tilemap.WorldToCell(transform.position);
+
+            // Kiểm tra xem có tile không
+            TileBase tile = tilemap.GetTile(cellPosition);
+            if (tile != null)
+            {
+                Vector3 worldPosition = tilemap.GetCellCenterWorld(cellPosition);
+
+                // Tạo đối tượng tại vị trí tile
+                Instantiate(objectToCreate, worldPosition, Quaternion.identity);
+
+                // Optional: Xóa tile để ngăn chặn tạo nhiều đối tượng tại cùng một vị trí
+                tilemap.SetTile(cellPosition, null);
+            }
         }
     }
 
