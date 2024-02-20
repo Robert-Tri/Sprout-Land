fe97fe142c0c922986e76f19518ec80e7f243c51
Đức Trí <ductri3122002@gmail.com>
2024-02-20 15:20:07 +0700

    update Map2 v3

--- Assets/DialogManager.cs
+++ Assets/DialogManager.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour
 {
     public GameObject currentDialog;
     // Thuộc tính tham chiếu tới Prefab của Dialog Question 
     public GameObject questionDialogPrefab;
+    public GameObject content;
+    public GameObject iconQuestion;
+    public GameObject iconChest;
+    public Button buttonA;
+    public Button buttonB;
+    public Sprite checkSprite;
+    public float delayBeforeDestroy = 2f; // Độ trễ trước khi hủy đối tượng
+
+    public Image uiImage; // Tham chiếu đến component Image trong Inspector
+    public Sprite correctSprite;
+    public Sprite wrongSprite;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,32 +32,40 @@ public class DialogManager : MonoBehaviour
         {
             questionDialogPrefab.SetActive(false);
         }
+        uiImage.gameObject.SetActive(false);
     }
-
-    
-
+ 
     // Hàm hiển thị Dialog Question
     public void ShowQuestionDialog()
     {
 
-        // Kiểm tra nếu đã có dialog được mở rồi thì đóng đi
-        if (currentDialog != null)
-        {
-            Destroy(currentDialog);
-        }
-
-        // Khởi tạo Prefab của Dialog Question
-        GameObject dialogObj = Instantiate(questionDialogPrefab);
-
-        // Lấy tham chiếu component QuestionDialog Script
-        QuestionDialog ctrl = dialogObj.GetComponent<QuestionDialog>();
-
-        // Thiết lập câu hỏi, có thể lấy ngẫu nhiên từ mảng các câu hỏi
-        ctrl.SetQuestion("Câu hỏi mẫu?");
-
-        // Lưu lại tham chiếu dialog hiện tại
-        currentDialog = dialogObj;
+        currentDialog = Instantiate(questionDialogPrefab);
+        currentDialog.SetActive(true);
 
     }
+    public void OnButtonAClick()
+    {
+        iconQuestion.SetActive(false);
 
+        uiImage.sprite = correctSprite;
+        uiImage.gameObject.SetActive(true);
+
+   
+        // Bắt đầu coroutine để hủy đối tượng sau một khoảng thời gian
+        StartCoroutine(DelayedDestroy());
+    }
+
+    private IEnumerator DelayedDestroy()
+    {
+        // Chờ trong khoảng thời gian đã đặt
+        yield return new WaitForSeconds(delayBeforeDestroy);
+
+        // Hủy đối tượng
+        Destroy(currentDialog);
+        iconChest.SetActive(true);
+    }
+    public void OnButtonBClick()
+    {
+        return;
+    }
 }
