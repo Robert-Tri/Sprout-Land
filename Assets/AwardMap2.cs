71cdb6d7f54b9f5eca6d87ec332ca7dc08f2eabe
Đức Trí <ductri3122002@gmail.com>
2024-02-27 15:10:24 +0700

    update Map2 v4

---
+++ Assets/AwardMap2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwardMap2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Tăng tiền khi nhặt
+
+            // Khi người chơi chạm vào, ẩn đi đối tượng
+            gameObject.SetActive(false);
+        }
+    }
+
+}
--- Assets/OpenChest.cs
+++ Assets/OpenChest.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class OpenChest : MonoBehaviour
 {
+    // Biến tĩnh để nhận giá trị countAnswer
+    private static int countAnswerReceived;
     protected Animator animator;
-    // Biến để xác định trạng thái của rương
+    public GameObject award1;
+    public GameObject award2;
+    public GameObject award3;
+    public GameObject award4;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        award1.SetActive(false);
+        award2.SetActive(false);
+        award3.SetActive(false);
+        award4.SetActive(false);
+
+
+        this.animator = GetComponent<Animator>();
+    }
     // Hàm được gọi khi người chơi va chạm vào rương
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Kiểm tra xem đối tượng va chạm có phải là người chơi không
         if (other.CompareTag("Player"))
         {
-            // Kiểm tra xem rương đã mở chưa       
+            // Kiểm tra xem rương đã mở chưa 
+            if (!animator.GetBool("isOpenChest"))
+            {
                 // Gọi hàm để mở rương
-                OpenChestFunction();      
+                OpenChestFunction();
+            }
         }
     }
 
@@ -26,12 +47,28 @@ public class OpenChest : MonoBehaviour
 
         // Đặt trạng thái rương thành đã mở
         animator.SetBool("isOpenChest", true);
-        // Bạn có thể thêm các hiệu ứng âm thanh, hình ảnh, hoặc thay đổi trạng thái đồ họa ở đây
+        if(countAnswerReceived > 1)
+        {
+            award1.SetActive(true);
+            award2.SetActive(true);
+        }
+        if (countAnswerReceived > 2)
+        {
+            award3.SetActive(true);
+        }
+        if (countAnswerReceived > 3)
+        {
+            award4.SetActive(true);
+
+        }
+
+
+
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public static void SetCountAnswer(int value)
     {
-        this.animator = GetComponent<Animator>();
+        countAnswerReceived = value;
     }
 }
+
