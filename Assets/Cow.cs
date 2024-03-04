15ed64f8deaba8922673f7791d1a416955a5c73d
Tran Hong Hung <bintran7121@gmail.com>
2024-03-04 21:31:05 +0700

    3/4

---
+++ Assets/Cow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Cow : MonoBehaviour
+{
+    private Animator animator;
+    public float moveSpeed = 3f; 
+    private Vector2 randomDirection;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        randomDirection = Random.insideUnitCircle.normalized;
+    }
+
+    void Update()
+    {
+        animator.SetBool("IsMoving", randomDirection.magnitude > 0.1f);
+        transform.Translate(randomDirection * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fence"))
+        {
+            ChangeDirection();
+        }
+    }
+
+    void ChangeDirection()
+    {
+        randomDirection = -randomDirection;
+    }
+}
